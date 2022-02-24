c32392dd95c5cdd9315d133478f0a8f582df5229
btshensfc <47489688+btshensfc@users.noreply.github.com>
2022-02-24 13:02:31 -0800

    Update timecalculator.go

--- timecalculator.go
+++ timecalculator.go
@@ -6,23 +6,7 @@ import ("fmt"
 		"strconv"
 )
 
-func main() {
-	fmt.Println("enter date of birth (MM/DD/YYYY): ")
-	var birthDate string
-	todayDate := time.Now().Format("01/02/2006")
-	fmt.Scanln(&birthDate)
-
-	test := strings.Split(todayDate, "/")
-	month, _ := strconv.Atoi(test[0])
-	day, _ := strconv.Atoi(test[1])
-	year, _ := strconv.Atoi(test[2])
-
-	test = strings.Split(birthDate, "/")
-	monthBirth, _ := strconv.Atoi(test[0])
-	dayBirth, _ := strconv.Atoi(test[1])
-	yearBirth, _ := strconv.Atoi(test[2])
-
-
+func calculation (month, day, year, monthBirth, dayBirth, yearBirth int) (int, int, int){
 	yearDiff := year - yearBirth
 
 	monthDiff := month - monthBirth
@@ -33,9 +17,47 @@ func main() {
 
 	dayDiff := day - dayBirth
 	if dayDiff < 0 {
-		dayDiff = (32 - dayDiff)
+		dayDiff += (32 - dayDiff)
 		monthDiff--
 	}
+
+	return monthDiff, dayDiff, yearDiff
+
+}
+
+func handleInput(s, sep string) []string {
+	repeat := true
+	var final []string
+
+	for repeat == true {
+		final = strings.Split(s, sep)
+		if len(final) == 1 {
+			fmt.Printf("Input incorrect! Please use %s as the seperators. \n", sep)
+			fmt.Scanln(&s)
+		} else {
+			repeat = false
+		}
+	}
+	return final
+}
+
+func main() {
+	fmt.Println("enter date of birth (MM/DD/YYYY): ")
+	todayDate := time.Now().Format("01/02/2006")
+	var birthDate string
+	fmt.Scanln(&birthDate)
+
+	test := handleInput(todayDate, "/")
+	month, _ := strconv.Atoi(test[0])
+	day, _ := strconv.Atoi(test[1])
+	year, _ := strconv.Atoi(test[2])
+
+	test = handleInput(birthDate, "/")
+	monthBirth, _ := strconv.Atoi(test[0])
+	dayBirth, _ := strconv.Atoi(test[1])
+	yearBirth, _ := strconv.Atoi(test[2])
+
+	monthDiff, dayDiff, yearDiff := calculation(month, day, year, monthBirth, dayBirth, yearBirth)
 
 	fmt.Printf("You are %d years, %d months, %d days old.", yearDiff, monthDiff, dayDiff)
 
