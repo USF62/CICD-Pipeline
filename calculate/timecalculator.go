9e09f82e1f7384e664375ecefd79a2ad3d5a6032
btshensfc <shen.braint@gmail.com>
2022-03-14 20:28:24 -0700

    edited certain goland stuff

---
+++ calculate/timecalculator.go
@@ -0,0 +1,41 @@
+package calculate
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Calculation(month, day, year, monthBirth, dayBirth, yearBirth int) (int, int, int) {
+	yearDiff := year - yearBirth
+
+	monthDiff := month - monthBirth
+	if monthDiff < 0 {
+		monthDiff += 12
+		yearDiff--
+	}
+
+	dayDiff := day - dayBirth
+	if dayDiff < 0 {
+		dayDiff += (32 - dayDiff)
+		monthDiff--
+	}
+
+	return monthDiff, dayDiff, yearDiff
+
+}
+
+func HandleInput(s, sep string) []string {
+	repeat := true
+	var final []string
+
+	for repeat == true {
+		final = strings.Split(s, sep)
+		if len(final) == 1 {
+			fmt.Printf("Input incorrect! Please use %s as the seperators. \n", sep)
+			fmt.Scanln(&s)
+		} else {
+			repeat = false
+		}
+	}
+	return final
+}
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-import ("fmt"
-		"time"
-		"strconv"
+import (
+	"fmt"
+	"github.com/venkatrajm815/USF62-Pipeline/calculate"
+	"strconv"
+	"time"
 )
-
 
 func main() {
 	fmt.Println("enter date of birth (MM/DD/YYYY): ")
@@ -12,17 +13,17 @@ func main() {
 	var birthDate string
 	fmt.Scanln(&birthDate)
 
-	test := handleInput(todayDate, "/")
+	test := calculate.HandleInput(todayDate, "/")
 	month, _ := strconv.Atoi(test[0])
 	day, _ := strconv.Atoi(test[1])
 	year, _ := strconv.Atoi(test[2])
 
-	test = handleInput(birthDate, "/")
+	test = calculate.HandleInput(birthDate, "/")
 	monthBirth, _ := strconv.Atoi(test[0])
 	dayBirth, _ := strconv.Atoi(test[1])
 	yearBirth, _ := strconv.Atoi(test[2])
 
-	monthDiff, dayDiff, yearDiff := calculation(month, day, year, monthBirth, dayBirth, yearBirth)
+	monthDiff, dayDiff, yearDiff := calculate.Calculation(month, day, year, monthBirth, dayBirth, yearBirth)
 
 	fmt.Printf("You are %d years, %d months, %d days old.", yearDiff, monthDiff, dayDiff)
 
