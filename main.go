a4f9b8d2627b12f4328b5012b84d031bbe4165c9
Venkatraj Mohan <venkatrajm815@gmail.com>
2022-03-25 16:14:31 +0530

    Fixed Files

--- main.go
+++ main.go
@@ -1,29 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"github.com/venkatrajm815/USF62-Pipeline/DaysToBday"
-	"strconv"
-	"strings"
-)
-
 func main() {
-	fmt.Println("Hi, Please input your birthday in the following format in numbers:\nmonth/date/year  Ex: 03/24/2000")
-	var bDay string
-	fmt.Scanln(&bDay)
-	daysToBirthday := DaysToBday.CalculateDaysToBday(DaysToBday.SplitBday(bDay))
-	splitBirthDay := strings.Split(bDay, "/")
-	month, error := strconv.Atoi(splitBirthDay[0])
-	DaysToBday.CheckError(error)
-	day, error := strconv.Atoi(splitBirthDay[1])
-	DaysToBday.CheckError(error)
-	year, error := strconv.Atoi(splitBirthDay[2])
-	DaysToBday.CheckError(error)
-
-	horoscope := DaysToBday.GetHoroscope(month, day)
-	zodiac := DaysToBday.GetZodiac(month, day, year)
-
-	fmt.Println(daysToBirthday)
-	fmt.Println(horoscope)
-	fmt.Println(zodiac)
 }
