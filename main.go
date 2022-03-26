99ecfb19fe01bec77b7aaa156d80e7541740eb48
Kassy Pak <kpak3@dons.usfca.edu>
2022-03-25 18:22:45 -0700

    added Chen's html code and created two "edit" and "view" files for taking in user input (edit) and returning information (view). Need to edit handlers to load the correct webpage.

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-	
+import (
+	"github.com/venkatrajm815/USF62-Pipeline/webapp"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("webapp/view/", webapp.MakeHandler(webapp.ViewHandler))
+	http.HandleFunc("webapp/edit/", webapp.MakeHandler(webapp.EditHandler))
+	http.HandleFunc("webapp/save/", webapp.MakeHandler(webapp.SaveHandler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
