fad4bcc0c51e0d14d578e7c80fa629491481a6bd
Kassy Pak <kpak3@dons.usfca.edu>
2022-04-07 17:12:36 -0700

    added ajax to home.html with a script for accepting backend data (go) to edit html page dynamically without reloading. need to fix up output

--- webapp/webapp.go
+++ webapp/webapp.go
@@ -7,16 +7,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 var client *redis.Client
 var templates *template.Template
 
+type Birthdate struct {
+	Month int
+	Day   int
+	Year  int
+}
+
 func Start() {
 	client = redis.NewClient(&redis.Options{
 		Addr: "localhost:8080",
 	})
-	templates = template.Must(template.ParseFiles("webapp/templates/home.html"))
+	templates = template.Must(template.ParseFiles("webapp/templates/home.html", "webapp/templates/results.html"))
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler).Methods("GET")
@@ -43,11 +51,23 @@ func resultsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		return
 	}
-	err := r.ParseForm()
-	if err != nil {
+	if err := r.ParseForm(); err != nil {
 		fmt.Print(err)
+		return
 	}
-	birthdate := r.PostFormValue("birthdate")
-	client.LPush("birthdate", birthdate)
+
+	userBD := strings.Split(r.FormValue("birthdate"), "-")
+	userYear, _ := strconv.Atoi(userBD[0])
+	userMonth, _ := strconv.Atoi(userBD[1])
+	userDay, _ := strconv.Atoi(userBD[2])
+
+	birthdate := Birthdate{
+		Month: userMonth,
+		Day:   userDay,
+		Year:  userYear,
+	}
+
+	w.Write([]byte(userBD[0]))
+	fmt.Print(birthdate)
 	http.Redirect(w, r, "/", 302)
 }
